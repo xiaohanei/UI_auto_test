b6bda45ec4adf95e8052e837f9f6c3292c928209
xiaohanwei <1398741287@qq.com>
2023-04-18 18:59:30 +0800

    优化获取jenkins上的参数

--- config/driver.py
+++ config/driver.py
@@ -12,11 +12,11 @@ BASE_DIR = "/Users/mac/Desktop/pythonProject/UI_auto_test"
 sys.path.append(BASE_DIR)
 from pubilc.tools.get_jenkins_parameter import get_jenkis_parameter
 def browser():
-    # chrome_options = Options()
-    # chrome_options.add_argument('--headless')
-    # driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver = webdriver.Chrome()
-    url = ReadFile().read_excel_not_include_header("UI_auto_test","config/test_environment_address.xlsx")[0][1]
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    #driver = webdriver.Chrome()
+    #url = ReadFile().read_excel_not_include_header("UI_auto_test","config/test_environment_address.xlsx")[0][1]
     for param in get_jenkis_parameter("http://localhost:8080/","UI_auto_test",'admin','11620ee242fbd91a1d247810ed518f479d'):
         if param['name'] =='environment':
             url = param['value']
